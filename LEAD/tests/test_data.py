6ebed496dd1c487abe8e66447ccec0b16a87a46f
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 15:32:13 +0000

    test_cleaner_data

--- LEAD/tests/test_data.py
+++ LEAD/tests/test_data.py
@@ -7,7 +7,7 @@ def test_load_data():
 
     df = pd.read_json('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/corse_insee.json', orient='records')
     assert not df.empty 
-    assert df.shape[1] == 4
+    assert df.shape[1] == 3
     assert df.shape[0] == 417
 
 
