2f7c97c08335ea2f784796f25f1bc562e429ddf9
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 15:50:25 +0000

    test_data_insee_2

--- LEAD/tests/conftets.py
+++ LEAD/tests/conftets.py
@@ -1,9 +1,8 @@
 import pytest
 import pandas as pd
-import numpy as np
 
 @pytest.fixture
 def data_insee():
-    return df = pd.read_json('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/corse_insee.json', orient='records')
-    
-
+    # Charger les données depuis l'URL JSON
+    df = pd.read_json('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/corse_insee.json', orient='records')
+    return df
