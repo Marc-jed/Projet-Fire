0c71f86b4e8ff668c93519ef74cd708a1c3071c6
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 14:46:39 +0000

    fichier_pas_vide

--- LEAD/tests/test_data.py
+++ LEAD/tests/test_data.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_load_data():
 
-    df = pd.read_json('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/corse_insee_toto.json', orient='records')
-    assert not df.empty, "df est vide" 
+    df = pd.read_json('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/corse_insee.json', orient='records')
+    assert df.empty 
 
     
