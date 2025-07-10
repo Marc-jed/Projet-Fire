b3bb3af74f454d7bb61fa8bd406a4001d76c9a80
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 16:17:23 +0000

    indentation

--- LEAD/tests/conftest.py
+++ LEAD/tests/conftest.py
@@ -8,6 +8,6 @@ def data_insee():
     return df
 
 @pytest.fixture
- def data_feu():
+def data_feu():
     df_feu = pd.read_csv('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/historique_incendies_avec_coordonnees.csv', sep=';', encoding='utf-8')   
     return df_feu
