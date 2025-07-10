b0d06817cae63626e21416dd7c1d5d631eb515ff
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 14:53:31 +0000

    test_shape_col_empty

--- LEAD/tests/test_data.py
+++ LEAD/tests/test_data.py
@@ -6,6 +6,14 @@ import pytest
 def test_load_data():
 
     df = pd.read_json('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/corse_insee.json', orient='records')
-    assert df.empty 
+    assert not df.empty 
+    assert df.shape[1] == 3
+    assert df.shape[0] == 417
 
+
+
+# def test_clean_data(sample_data):
+#     cleaned_df = clean_data(sample_data)
+#     assert cleaned_df.isnull().sum().sum() == 0  # Ensure no NaN values
+#     assert cleaned_df["feature"].dtype == float  # Ensure correct data type
     
