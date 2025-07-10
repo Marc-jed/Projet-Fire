4ad9afc638b0345d20628d48c068743188f1243f
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 16:16:16 +0000

    data_feu

--- LEAD/tests/test_data.py
+++ LEAD/tests/test_data.py
@@ -6,8 +6,15 @@ def test_load_data(data_insee):
     assert data_insee.shape[1] == 3
     assert data_insee.shape[0] == 417
 
-
-
 def test_cleaner_data(data_insee):
     assert data_insee.isnull().sum().sum() == 0
-   
+
+def test_load_data_feu(data_feu):
+    assert not data_feu.empty
+    assert data_feu.shape[1] == 19
+
+def test_cleaner_data_feu(data_feu):
+    assert data_feu.isnull().sum()[data_feu.columns[0]]==0
+
+
+
