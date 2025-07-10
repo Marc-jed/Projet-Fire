35a6d20cd16ba576a1d354b751894637a58e3f3c
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 16:05:31 +0000

    test_cleaner_data

--- LEAD/tests/test_data.py
+++ LEAD/tests/test_data.py
@@ -3,7 +3,11 @@ import pytest
 
 def test_load_data(data_insee):
     assert not data_insee.empty
+    assert data_insee.shape[1] == 3
+    assert data_insee.shape[0] == 417
 
 
 
-  
+def test_cleaner_data(data_insee):
+    assert data_insee.isnull().sum().sum() == 0
+   
