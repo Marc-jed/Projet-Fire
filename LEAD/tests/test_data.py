279d8cce1be43fba7673cbb2dc3271644f000daf
Nath-data13 <nathaliedevogelaere@gmail.com>
2025-07-10 14:34:33 +0000

    test_data.py

---
+++ LEAD/tests/test_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pytest
+
+
+
+def test_load_data():
+
+    df = pd.read_json('https://fireprojectbislead.s3.us-east-1.amazonaws.com/dataset/corse_insee.json', orient='records')
+    assert not df.empty, "df est vide" 
+
+    
